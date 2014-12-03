32262e33c4701480af43645aa7ec3c520e49b627
thor <thor.qin@gmail.com>
2014-12-03 17:00:57 +0800

    * Bug fixed: Checkbox/Radiobox active backgroud-color didn't restore when rendered in firefox.

--- tui/tui.ctrl.tab.ts
+++ tui/tui.ctrl.tab.ts
@@ -7,7 +7,7 @@ module tui.ctrl {
 
 		constructor(el?: HTMLElement) {
 			super("div", Tab.CLASS, el);
-
+			var self = this;
 			var removeList = [];
 			var activeIndex = 0;
 			for (var i = 0; i < this[0].childNodes.length; i++) {
@@ -19,25 +19,28 @@ module tui.ctrl {
 					this._buttons.push(button);
 					if (button.checked())
 						activeIndex = this._buttons.length - 1;
-					button.on("check", this.checkPage);
+					button.on("check", function(data){
+						self.checkPage(data.ctrl);
+					});
 				} else
 					removeList.push(child);
 			}
-			for (var i = 0; i < removeList.length; i++) {
-				tui.removeNode(removeList[i]);
-			}
+			//for (var i = 0; i < removeList.length; i++) {
+			//	tui.removeNode(removeList[i]);
+			//}
 			this.at(activeIndex).checked(true);
 		}
 
-		private checkPage(data: any) {
-			var tabId = data.ctrl.attr("data-tab");
+		private checkPage(button: any) {
+			var tabId = button.attr("data-tab");
 			tabId = "#" + tabId;
-			if (data.ctrl.checked()) {
+			if (button.checked()) {
 				$(tabId).removeClass("tui-hidden");
 				tui.ctrl.initCtrls($(tabId)[0]);
 			} else {
 				$(tabId).addClass("tui-hidden");
 			}
+			this.fire("active", {index: this._buttons.indexOf(button), text: button.text()});
 		}
 
 		at(index: number): Radiobox {
@@ -46,13 +49,25 @@ module tui.ctrl {
 			else
 				return null;
 		}
+		
+		count(): number {
+			return this._buttons.length;
+		}
 
-		add(name: string): Tab;
-		add(name: string, index: number): Tab;
-		add(name: string, index?: number): Tab {
+		add(name: string): Radiobox;
+		add(name: string, index: number): Radiobox;
+		add(name: string, index?: number): Radiobox {
+			var self = this;
+			if (typeof index === "number") {
+				if (index >= this._buttons.length)
+					return null;
+			}			
 			var button = tui.ctrl.radiobox();
 			button.text(name);
-			button.on("check", this.checkPage);
+			button.group(this._tabId);
+			button.on("check", function(data){
+				self.checkPage(data.ctrl);
+			});
 			if (typeof index === tui.undef) {
 				this[0].appendChild(button[0]);
 				this._buttons.push(button);
@@ -60,17 +75,21 @@ module tui.ctrl {
 				this[0].insertBefore(button[0], this.at(index)[0]);
 				this._buttons.splice(index, 0, button);
 			}
-			return this;
+			return button;
 		}
 
-		remove(index: number): Tab {
+		remove(index: number): Radiobox {
 			var button = this.at(index);
 			if (button) {
-				button.off("check", this.checkPage);
 				this._buttons.splice(index, 1);
 				tui.removeNode(button[0]);
+				if (index < this._buttons.length)
+					this.active(index);
+				else if (this._buttons.length > 0)
+					this.active(this._buttons.length - 1);
+				return button;
 			}
-			return this;
+			return null;
 		}
 
 		active(): number;
@@ -80,6 +99,7 @@ module tui.ctrl {
 				var button = this.at(index);
 				if (button) {
 					button.checked(true);
+					this.checkPage(button);
 				}
 				return this;
 			} else {
